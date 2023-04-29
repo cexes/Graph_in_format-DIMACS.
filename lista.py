39de087c4a32d508e92ed64340d0444069e1ac4b
cexes <mottacesar001@gmail.com>
2023-04-29 06:01:15 -0300

    NEW

--- lista.py
+++ lista.py
@@ -1,23 +1,16 @@
 class Lista:
+ 
   def __init__(self,vertices):
      self.vertices = vertices
      self.grafo = [[] for i in range(self.vertices)]
      
-    
-  def adiconar_aresta(self, u,v,peso):
-     self.grafo[u-1].append([v,peso])
 
-  def mostra_lista(self):
+  def adiconar_arestas(self, u,destino,peso):
+     self.grafo[u-1].append([destino,peso])
+
+  def mostrar_lista(self):
      for i in range(self.vertices):
         print(f'{i+1}:',end= ' ')
         for j in self.grafo[i]:
-           print(f'{j} ->',end=' ')
+           print(f'{j}: ->',end=' ')
         print("")
-
-g = Lista(4)
-g.adiconar_aresta(1,2,0)
-g.adiconar_aresta(1,4,2)
-g.adiconar_aresta(4,1,1)
-g.adiconar_aresta(3,2,1)
-g.adiconar_aresta(3,2,2)
-g.mostra_lista()
--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from lista import Lista
-from matriz import Matriz
-class Main:
-  
 
+class Main:
   origemdestinopeso = []
   with open('arquivo.txt')as files:
     for l in files:
        if l.startswith('p'):
           _, v1,a1 = l.split()
           arestas = (int(v1))
-          vertices = (int(a1))
-          Lista(vertices)
-           
+          vertices = (int(a1))        
        if l.startswith('e'):
           descarte, origem,destino,peso = l.split()
-          int(origem)
-          int(destino)
-          int(peso)
-          #print(origem,destino,peso)
-
+          o = int(origem)
+          d =int(destino)
+          p =int(peso)
+          lista = Lista(2)
+          lista.adiconar_arestas(1,2,3)
+          lista.mostrar_lista()
